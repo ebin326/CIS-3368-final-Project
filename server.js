e7c0ac2ca9e5e0051aa81754ed97ac59bf98f025
Ismael <ismaelturtle@gmail.com>
2024-08-07 20:48:14 -0500

    stuffs

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express'); // Serves our pages to a specified port
+const app = express();
+const cors = require('cors');
+const axios = require('axios').create({baseURL: "http://127.0.0.1:5000"}); // Makes external API calls into our frontend server
+const bodyParser  = require('body-parser');
+const path = require('path');
+
+app.use(bodyParser.urlencoded());
+app.use(express.static('Frontend'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+// Set the view engine to ejs & defines views directory
+app.set('views', path.join(__dirname, 'views/'));
+app.set('view engine', 'ejs');
+
+const headers = {
+    'content-type': 'application/json'
+}
+
+// Dashboard
+app.get('/', function(req, res) {
+    res.render('dashboard.ejs', {
+    });
+});
+
+app.listen(8000);
+console.log('Front-End server running on port 8000!');
